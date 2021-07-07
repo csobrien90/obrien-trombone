d1b9fb8ed6ca499f81ea3fcc7b8f72495f34ca7f
Chad O'Brien <obrien.music@gmail.com>
2021-07-07 19:13:39 -0400

    add footer and move header and footer to template

--- docs/js/hamburger.js
+++ docs/js/hamburger.js
@@ -4,24 +4,27 @@ const topLine = document.getElementsByClassName('top')[0];
 const bottomLine = document.getElementsByClassName('bottom')[0];
 const middleLine = document.getElementsByClassName('middle')[0];
 
+function burgerToX() {
+    middleLine.style.height = '0';
+    topLine.style.transform = 'translateY(7.5px) rotate(45deg)';
+    bottomLine.style.transform = 'translateY(-7.5px) rotate(-45deg)';
+}
+
+function xToBurger() {
+    topLine.style.transform = 'rotate(0) translateY(0)';
+    bottomLine.style.transform = 'rotate(0) translateY(0)';
+    setTimeout(() => {middleLine.style.height = '5px';}, 100);
+}
+
 //open and close navMenu with hamburger button
 
 hamburger.addEventListener('click', () => {
     if (navMenu.style.height == 0 || navMenu.style.height == '0px') {
         navMenu.style.height = '194px';
-        
-        //hamburger menu animation to 'x'
-        middleLine.style.height = '0';
-        topLine.style.transform = 'translateY(7.5px) rotate(45deg)';
-        bottomLine.style.transform = 'translateY(-7.5px) rotate(-45deg)';
-
+        burgerToX();
     } else {
         navMenu.style.height = 0;
-
-        //hamburger menu 'x' animation back to original design
-        topLine.style.transform = 'rotate(0) translateY(0)';
-        bottomLine.style.transform = 'rotate(0) translateY(0)';
-        setTimeout(() => {middleLine.style.height = '5px';}, 100);
+        xToBurger();        
     }
 })
 
@@ -30,7 +33,9 @@ hamburger.addEventListener('click', () => {
 window.addEventListener('resize', () => {
     if (window.innerWidth > 599) {
         navMenu.style.height = 'unset';
+        xToBurger();
     } else if (window.innerWidth < 600 && navMenu.style.height !== 0) {
         navMenu.style.height = 0;
+        xToBurger();
     }
 })
---
+++ docs/js/template.js
@@ -0,0 +1,31 @@
+const header = document.getElementsByTagName('header')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+header.innerHTML = `
+    <div id="logo">
+    <h1>Chad O'Brien</h1>
+    <img src="images/logo.png" alt="an abstract illustration of a trombone">
+    </div>
+
+    <nav>
+    <ul>
+        <li><a href="about.html">About</a></li>
+        <li><a href="media.html">Media</a></li>
+        <li><a href="connect.html">Connect</a></li>
+        <li><a href="resources.html">Resources</a></li>
+        <li><a href="blog.html">Blog</a></li>
+    </ul>
+    </nav>
+
+    <div id="hamburger-icon">
+    <div class="top line"></div>
+    <div class="middle line"></div>
+    <div class="bottom line"></div>
+    </div>
+`;
+
+const year = new Date().getFullYear();
+
+footer.innerHTML = `
+    &copy; Copyright ${year} - Chad O'Brien
+`;
