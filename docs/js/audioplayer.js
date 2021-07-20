7cba7968f0767e482e5e21b8583458c1a0045d48
Chad O'Brien <obrien.music@gmail.com>
2021-07-20 08:05:22 -0400

    toggle play and pause buttons for audio player

--- docs/js/audioplayer.js
+++ docs/js/audioplayer.js
@@ -5,6 +5,7 @@ const pause = document.getElementById('pause');
 const turnUp = document.getElementById('volume-up');
 const turnDown = document.getElementById('volume-down');
 const trackTitle = document.getElementById('track-title');
+const trackDescription = document.getElementById('track-description');
 const trackTime = document.getElementById('track-time');
 
 function showTime() {
@@ -22,14 +23,33 @@ function secondsToMinutes(seconds) {
 
 setInterval(showTime, 200);
 
-play.addEventListener('click', () => {selectedTrack.play(); showTime})
-pause.addEventListener('click', () => {selectedTrack.pause(); showTime})
+play.addEventListener('click', () => {
+    selectedTrack.play();
+    showTime;
+    play.style.display = "none";
+    pause.style.display = "block";
+})
+
+pause.addEventListener('click', () => {
+    selectedTrack.pause();
+    showTime;
+    pause.style.display = "none";
+    play.style.display = "block";
+})
 
 for (let i = 0; i < tracks.length; i++) {
     tracks[i].addEventListener('click', (event) => {
         var filename = event.target.parentNode.id;
         var title = event.target.parentNode.firstElementChild.innerHTML;
+        var description = event.target.parentNode.lastElementChild.innerHTML;
+        
+        if (play.style.display == "none") {
+            pause.style.display = "none";
+            play.style.display = "block";
+        }
+        
         selectedTrack.src = `files/recordings/${filename}.mp3`
         trackTitle.innerHTML = title;
+        trackDescription.innerHTML = description;
     })
 }
