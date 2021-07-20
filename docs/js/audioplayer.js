6e9b01c0552e051b5cdcbfb4c77640120ccf0183
Chad O'Brien <obrien.music@gmail.com>
2021-07-19 23:24:10 -0400

    create basic custom audio player interface

--- docs/js/audioplayer.js
+++ docs/js/audioplayer.js
@@ -1,9 +1,30 @@
-const audioPlayer = document.getElementById('audio-player');
+const track = document.getElementById('player');
 const tracks = document.getElementsByClassName('audio-track');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const turnUp = document.getElementById('volume-up');
+const turnDown = document.getElementById('volume-down');
+const trackTime = document.getElementById('track-time');
+
+function showTime() {
+    trackTime.innerHTML = `${secondsToMinutes(Math.floor(player.currentTime))} / ${secondsToMinutes(Math.floor(player.duration))}`;
+}
+
+function secondsToMinutes(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds - minutes * 60;
+    let formattedSeconds = ('0' + remainingSeconds).slice(-2)
+    return(`${minutes}:${formattedSeconds}`)
+}
+
+setInterval(showTime, 1000);
+
+play.addEventListener('click', () => {track.play(); showTime})
+pause.addEventListener('click', () => {track.pause(); showTime})
 
 for (let i = 0; i < tracks.length; i++) {
     tracks[i].addEventListener('click', (event) => {
         const filename = event.srcElement.parentNode.id;
-        audioPlayer.innerHTML = `<audio controls><source src="files/recordings/${filename}.mp3" type="audio/mpeg"></audio>`
+        track.src = `files/recordings/${filename}.mp3`
     })
 }
