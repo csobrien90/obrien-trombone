8850bf7ba45be9f0dfed2bed48217d85c0518084
Chad O'Brien <obrien.music@gmail.com>
2021-07-21 21:13:46 -0400

    fix hover js to only function when window width is 800px or greater

--- docs/js/hover-link.js
+++ docs/js/hover-link.js
@@ -3,15 +3,17 @@ const links = document.getElementsByTagName("a");
 for (let i = 0; i < links.length; i++) {
     
     links[i].addEventListener('mouseover', (event) => {
-        if (event.target.parentElement.localName == "nav") {
+        if (event.target.parentElement.localName == "nav" && window.innerWidth > 799) {
             event.target.lastElementChild.style.maxHeight = "100px";
             setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px 20px";}, 100);
         }
     })
 
     links[i].addEventListener('mouseleave', (event) => {
-        event.target.lastElementChild.style.maxHeight = "0";
-        setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px";}, 100);
+        if (window.innerWidth > 799) {
+            event.target.lastElementChild.style.maxHeight = "0";
+            setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px";}, 100);
+        }
     })
 }
 
