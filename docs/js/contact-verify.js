359beeb1e608e3436c1ceebcbdd82a58490926d8
Chad O'Brien <obrien.music@gmail.com>
2021-07-26 00:06:44 -0400

    add simple math equation for verification of humanity

--- docs/js/contact-verify.js
+++ docs/js/contact-verify.js
@@ -1,5 +1,6 @@
 const email = document.getElementById('email');
 const emailVerification = document.getElementsByClassName('verify')[0];
+const humanityVerification = document.getElementsByClassName('simple-math')[0];
 
 email.addEventListener('input', () => {
     var validity = validateEmail(email.value);
@@ -8,7 +9,33 @@ email.addEventListener('input', () => {
     } else {emailVerification.innerHTML = "";}
 })
 
+function generateMath() {
+    var numb1 = getRandomInt(9);
+    var numb2 = getRandomInt(9);
+    var answer = numb1 + numb2;
+    humanityVerification.innerHTML = `
+        Please verify you are not a bot: ${numb1} + ${numb2} = <input type="text" id="math-answer">
+    `;
+    return(answer);
+}
+
+function getRandomInt(max) {
+    let int = 1 + Math.floor(Math.random() * max);
+    return(int);
+  }
+
 function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+var answer = generateMath();
+
+const mathAnswer = document.getElementById('math-answer');
+const submitButton = document.getElementById('form-submit');
+
+mathAnswer.addEventListener('input', () => {
+    if (+mathAnswer.value == answer) {
+        submitButton.disabled = false;
+    } else {submitButton.disabled = true}
+})
