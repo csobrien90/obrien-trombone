a5c83268ae5007d9252bcfb201fe9c4ba230ae2c
Chad O'Brien <obrien.music@gmail.com>
2021-07-30 11:42:18 -0400

    update contact form php script

--- docs/php/contact-form.php
+++ docs/php/contact-form.php
@@ -1,12 +1,21 @@
 <?php
 
+function console_log( $data ){
+    echo '<script>';
+    echo 'console.log('. json_encode( $data ) .')';
+    echo '</script>';
+}
+
+$to = 'obrien.music@gmail.com';
+$subject = 'New Message from obrientrombone.com Contact Form!';
 $name = $_POST['contactName'];
 $email = $_POST['contactEmail'];
-$message = $_POST['contactMessage'];
+$message = wordwrap($_POST['contactMessage']);
 
-$body = "Name: " . $name . "\n" . "Email: " . $email . "\n\n" . "Message: " . wordwrap($message);
+$body = "Name: $name\nEmail: $email\n\nMessage: $message";
 
-mail('obrien.music@gmail.com', 'New Message from obrientrombone.com Contact Form!', $body);
+mail($to, $subject, $body);
 header("Location: ../connect.html");
+
 
 ?>
