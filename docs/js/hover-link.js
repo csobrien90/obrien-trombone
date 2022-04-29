8291a9acc0954705c3efbdf7bcfbb0d1322d70f1
Chad OBrien <obrien.music@gmail.com>
2022-04-28 21:18:50 -0400

    move favicon to the right place and make landing page hover effects accessible

--- docs/js/hover-link.js
+++ docs/js/hover-link.js
@@ -1,25 +1,40 @@
 //Connect to the DOM
 
-const links = document.getElementsByTagName("a");
+const links = document.querySelectorAll("a");
 
 //Event listeners for hover effect that expands link descriptions
 
-for (let i = 0; i < links.length; i++) {
+links.forEach(link => {
     
-    links[i].addEventListener('mouseover', (event) => {
+    link.addEventListener('mouseover', (event) => {
         if (event.target.parentElement.localName == "nav" && window.innerWidth > 799) {
             event.target.lastElementChild.style.maxHeight = "100px";
             setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px 20px";}, 100);
         }
     })
 
-    links[i].addEventListener('mouseleave', (event) => {
+    link.addEventListener('mouseleave', (event) => {
         if (window.innerWidth > 799) {
             event.target.lastElementChild.style.maxHeight = "0";
             setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px";}, 100);
         }
     })
-}
+
+    link.addEventListener('focusin', (event) => {
+        if (event.target.parentElement.localName == "nav" && window.innerWidth > 799) {
+            event.target.lastElementChild.style.maxHeight = "100px";
+            setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px 20px";}, 100);
+        }
+    })
+
+    link.addEventListener('focusout', (event) => {
+        if (window.innerWidth > 799) {
+            event.target.lastElementChild.style.maxHeight = "0";
+            setTimeout(() => {event.target.lastElementChild.style.padding = "0 20px";}, 100);
+        }
+    })
+
+});
 
 //Compensate for hover style changes when resizing browser
 
