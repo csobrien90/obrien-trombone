c249340ebd789b404f7722a4810d463d0b0d0971
Chad OBrien <obrien.music@gmail.com>
2022-06-12 01:37:05 -0400

    implement reCaptcha for contact form

--- docs/js/contact-verify.js
+++ docs/js/contact-verify.js
@@ -1,57 +1,44 @@
 //Initial DOM connection
 const email = document.getElementById('email');
 const emailVerification = document.getElementsByClassName('verify')[0];
-const humanityVerification = document.getElementsByClassName('simple-math')[0];
 const emailStatus = document.getElementsByClassName('email-status')[0];
 
 //Event listener for email validation
 
 email.addEventListener('input', () => {
-    var validity = validateEmail(email.value);
-    if (!validity && email.value !== "") {
-        emailVerification.innerHTML = "Please enter a valid email";
-        emailStatus.src = "images/email-x.png";
-        emailStatus.style.display = "inline-block";
-    } else if (validity && email.value !== "") {
-        emailVerification.innerHTML = "";
-        emailStatus.src = "images/email-check.png";
-    } else {
-        emailVerification.innerHTML = "";
-        emailStatus.style.display = "none";
-    }
+	var validity = validateEmail(email.value);
+	if (!validity && email.value !== "") {
+		emailVerification.innerHTML = "Please enter a valid email";
+		emailStatus.src = "images/email-x.png";
+		emailStatus.style.display = "inline-block";
+	} else if (validity && email.value !== "") {
+		emailVerification.innerHTML = "";
+		emailStatus.src = "images/email-check.png";
+	} else {
+		emailVerification.innerHTML = "";
+		emailStatus.style.display = "none";
+	}
 })
 
 //Functions
 
 function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
 
-function generateMath() {
-    var numb1 = getRandomInt(9);
-    var numb2 = getRandomInt(9);
-    var answer = numb1 + numb2;
-    humanityVerification.innerHTML = `
-        Please verify you are not a bot: ${numb1} + ${numb2} = <input type="text" id="math-answer">
-    `;
-    return(answer);
-}
+// Form handling
+document.getElementById("contact-form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	const request = new FormData(e.target);
 
-function getRandomInt(max) {
-    let int = 1 + Math.floor(Math.random() * max);
-    return(int);
-}
-
-//Insert simple math equation as anti-bot measure
-
-var answer = generateMath();
-
-const mathAnswer = document.getElementById('math-answer');
-const submitButton = document.getElementById('form-submit');
-
-mathAnswer.addEventListener('input', () => {
-    if (+mathAnswer.value == answer) {
-        submitButton.disabled = false;
-    } else {submitButton.disabled = true}
-})
+	fetch('../php/contact-form.php', {
+	  method: 'POST',
+	  body: request
+	})
+	.then((response) => {return response.json();})
+	.then((response) => {
+		alert(response.message);
+		window.location.reload();
+	});
+});
--- docs/php/contact-form.php
+++ docs/php/contact-form.php
@@ -1,25 +1,45 @@
 <?php
 
-function console_log( $data ){
-    echo '<script>';
-    echo 'console.log('. json_encode( $data ) .')';
-    echo '</script>';
-}
+require_once '../../define.php';
 
-$honeypot = $_POST['honeyPot'];
+// Honey pot verification
+$honeypot = isset($_POST['honeyPot']) && ( strlen(htmlspecialchars($_POST['honeyPot']) ) > 0 );
+if ( $honeypot ) {
+	echo '{"message":"Get your fingers out of my honey pot!"}';
+	die();
+}; 
+
+// Sanitize POST data
+$name = htmlspecialchars($_POST['contactName']);
+$email = filter_var($_POST['contactEmail'], FILTER_SANITIZE_EMAIL);
+$message = wordwrap(htmlspecialchars($_POST['contactMessage']));
+$recaptcha_response = $_POST['g-recaptcha-response'];
+
+// Set email params
 $to = 'chad@obrientrombone.com';
 $subject = 'New Message from obrientrombone.com Contact Form!';
-$name = $_POST['contactName'];
-$email = $_POST['contactEmail'];
-$message = wordwrap($_POST['contactMessage']);
-
 $body = "Name: $name\nEmail: $email\n\nMessage: $message";
 
-if (!$honeypot) {
-    mail($to, $subject, $body);
+// Verify reCaptcha
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify?secret='.RECAPTCHA_SECRET_KEY.'&response='.$recaptcha_response);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$response = json_decode($response);
+
+if($response->success) {
+	// Successful verification - send email
+	$mail_sent = mail($to, $subject, $body);
+	$message = $mail_sent ? 'Thank you for reaching out! I will be in touch soon.' : 'Hmm - something went wrong. Please refresh the page and try again.';    
+} else {
+	// Failed the reCaptcha
+	$message = 'reCAPTHCA verification failed, please try again.';
 }
 
-header("Location: ../connect.html");
-
+header('Content-Type: application/json');
+// echo '{"message":"'.$message.'"}';
+echo '{"message":"'.$message.'"}';
+die();
 
 ?>
